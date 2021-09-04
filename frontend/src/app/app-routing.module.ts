b6eb3194c3cdc5abf401514cd407b063fa3eb120
BruceAndyLee <andruha34@mail.ru>
2021-09-04 12:34:48 +0300

    front-branch

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: AuthComponent
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./components/profile/profile.module').then( m => m.ProfilePageModule)
+  },
+  {
+    path: 'purchases',
+    loadChildren: () => import('./components/purchases/purchases.module').then( m => m.PurchasesPageModule)
+  },
+  {
+    path: 'edit-profile',
+    loadChildren: () => import('./components/edit-profile/edit-profile.module').then( m => m.EditProfilePageModule)
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
